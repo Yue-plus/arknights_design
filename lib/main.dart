a1056ef54c35ae3a7a1ec5839a5fedfb5e78ffa1
Yue-plus <Yue_plus@foxmail.com>
2022-02-21 19:45:42 +0800

    flutter create arknights_design

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Arknights Design',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 32),
+      ),
+    ),
+  );
+}
