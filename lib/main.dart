2412fc771d83a687a0474b312ea2a8fa25ca4e09
Yue-plus <Yue_plus@foxmail.com>
2022-06-08 15:02:43 +0800

    add dev_dependencies: fluent_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:arknights_design/arknights_design.dart';
 
-void main() => runApp(const ArknightsApp(
-  home: Center(child: Text('Arknights Design')),
-));
+void main() {
+  runApp(const ArknightsApp(
+    title: 'Arknights Design',
+    home: ArknightsPageScaffold(
+      appBar: SizedBox(child: Text('AppBar')),
+      body: Center(child: Text('Arknights Design')),
+    ),
+  ));
+}
