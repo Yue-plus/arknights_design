317822bcd04c01736c210066d9395537de412f33
Yue-plus <Yue_plus@foxmail.com>
2022-06-09 12:02:03 +0800

    update use ArknightsAppBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ void main() {
   runApp(const ArknightsApp(
     title: 'Arknights Design',
     home: ArknightsPageScaffold(
-      appBar: SizedBox(child: Text('AppBar')),
+      appBar: ArknightsAppBar(
+        leading: Text('◬'),
+        title: Text('Arknights Design'),
+        actions: [
+          Padding(padding: EdgeInsets.all(8), child: Text('▣')),
+          Padding(padding: EdgeInsets.all(8), child: Text('◆')),
+          Padding(padding: EdgeInsets.all(8), child: Text('▶')),
+        ],
+      ),
       body: Center(child: Text('Arknights Design')),
     ),
   ));
