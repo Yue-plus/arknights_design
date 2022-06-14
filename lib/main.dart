b34ee3660f6787fd5b410425fc50923988f80534
Yue-plus <Yue_plus@foxmail.com>
2022-06-14 15:08:58 +0800

    use ArknightsIcons

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
 import 'package:arknights_design/arknights_design.dart';
+import 'package:fluent_ui/fluent_ui.dart' show FluentIcons;
+
+Widget _rowIcon(IconData icon) {
+  return Padding(
+    padding: const EdgeInsets.all(8),
+    child: Icon(icon, size: 64, color: ArknightsColors.white),
+  );
+}
 
 void main() {
-  runApp(const ArknightsApp(
+  runApp(ArknightsApp(
     title: 'Arknights Design',
     home: ArknightsPageScaffold(
-      appBar: ArknightsAppBar(
-        leading: Text('◬'),
+      appBar: const ArknightsAppBar(
+        leading: Icon(FluentIcons.add, color: ArknightsColors.white),
         title: Text('Arknights Design'),
         actions: [
-          Padding(padding: EdgeInsets.all(8), child: Text('▣')),
-          Padding(padding: EdgeInsets.all(8), child: Text('◆')),
-          Padding(padding: EdgeInsets.all(8), child: Text('▶')),
+          Icon(FluentIcons.plug_connected, color: ArknightsColors.white),
+          Icon(FluentIcons.pill, color: ArknightsColors.white),
+          Icon(FluentIcons.release_gate, color: ArknightsColors.white),
         ],
       ),
-      body: Center(child: Text('Arknights Design')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Arknights Design',
+            style: TextStyle(
+              color: ArknightsColors.white,
+              fontSize: 64,
+              fontWeight: FontWeight.normal,
+              decoration: TextDecoration.none,
+            ),
+          ),
+          const SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _rowIcon(ArknightsIcons.caster),
+              _rowIcon(ArknightsIcons.medic),
+              _rowIcon(ArknightsIcons.pioneer),
+              _rowIcon(ArknightsIcons.sniper),
+              _rowIcon(ArknightsIcons.special),
+              _rowIcon(ArknightsIcons.support),
+              _rowIcon(ArknightsIcons.tank),
+              _rowIcon(ArknightsIcons.warrior),
+            ],
+          )
+        ],
+      ),
     ),
   ));
 }
